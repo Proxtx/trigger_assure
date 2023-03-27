652eaf3c894b51182c13de52af9eebd0f773cda9
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-27 19:34:14 +0200

    Async

--- handler.js
+++ handler.js
@@ -1,8 +1,8 @@
 const triggerConfig = triggerGui.getElementsByClassName("triggerConfig")[0];
 const times = triggerGui.getElementsByClassName("timesInput")[0];
 
-getTriggerConfiguration(() => {
-  let config = triggerConfig.component.getTriggerConfiguration();
+getTriggerConfiguration(async () => {
+  let config = await triggerConfig.component.getTriggerConfiguration();
 
   return {
     text: "assure " + config.text + " " + times.component.value + " times.",
